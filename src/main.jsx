647ef1806393201156818b5f490cff3193147b4d
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-05-11 20:23:42 +0300

    add script and config for gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,18 +6,21 @@ import './index.css';
 import MainLayout from './routes/mainLayout';
 import Posts from './routes/posts';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      {
-        path: '/posts',
-        element: <Posts />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <MainLayout />,
+      children: [
+        {
+          path: '/posts',
+          element: <Posts />,
+        },
+      ],
+    },
+  ],
+  { basename: '/new-routing-react' }
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
